ff78a4a908e25a5e9476958c77e891a7513d44ec
ArianeJunkes <aranejunkes312@gmail.com>
2023-06-23 14:22:12 -0300

    Permissões a nivel de ViewSet

--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from livraria.models import Autor, Categoria, Editora, Livro
@@ -6,10 +7,12 @@ from livraria.serializers import AutorSerializer, CategoriaSerializer, EditoraSe
 class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+    permission_classes = [IsAuthenticated]
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    permission_classes = [IsAuthenticated]
 
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
